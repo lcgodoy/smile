30c579e38f5ae010f811cd1b2875e29f001044ce
lcgodoy <lucasdac.godoy@gmail.com>
2021-04-19 14:52:48 -0400

    adding a function to calculate distance matrices and speeding up the

--- src/dists-aux.cpp
+++ src/dists-aux.cpp
@@ -6,6 +6,46 @@
 // [[Rcpp::plugins(cpp11)]]
 using namespace Rcpp;
 
+//' @name eucl_aux
+double eucl_aux(x, y) {
+  return sqrt( x*x + y*y );
+}
+
+//' @title Creatin a distance matrix
+//' 
+//' @description Internal use. For now it only supports euclidean distance. (May
+//'   import parallelDist in the future).
+//'
+//' @param my_mat a matrix representing a grid of points.
+//' 
+// [[Rcpp::export]]
+arma::mat distmat(const arma::mat& my_mat) {
+  int n = my_mat.n_rows, k = 0;
+  arma::vec aux( n * (n + 1) * .5,
+		 arma::fill::zeros);
+
+  arma::mat out(n, n, arma::fill::zeros);
+  
+  arma::uvec lw_idx = arma::trimatl_ind( arma::size(out) );
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      if(i == j) {
+	aux(k) = 0;
+      } else {
+        aux(k) = eucl_aux( my_mat(i, 0) - my_mat(j, 0),
+			   my_mat(i, 1) - my_mat(j, 1)  );
+      }
+      k += 1;
+    }
+  }
+ 
+  out.elem(lw_idx) = aux;
+  out = arma::symmatl(out);
+  
+  return out;
+}
+
 //' @title Pairwise distances between matrices
 //' 
 //' @description Internal use.
@@ -16,23 +56,17 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 arma::mat crossdist(const arma::mat& m1, const arma::mat& m2) {
 
-  int nrow1 = m1.n_rows, nrow2 = m2.n_rows, ncol = m1.n_cols;
- 
-  if (m1.n_cols != m2.n_cols) {
-    throw std::runtime_error("matrices with different number of columns");
-  }
+  int nrow1 = m1.n_rows, nrow2 = m2.n_rows;
  
   arma::mat out(nrow1, nrow2, arma::fill::zeros);
 
-  for (int r1 = 0; r1 < nrow1; r1++) {
-    for (int r2 = 0; r2 < nrow2; r2++) {
-      double total = 0;
-      for (int c12 = 0; c12 < ncol; c12++) {
-        total += pow(m1(r1, c12) - m2(r2, c12), 2);
-      }
-      out(r1, r2) = sqrt(total);
+  for (int i = 0; i < nrow1; i++) {
+    for (int j = 0; j < nrow2; j++) {
+      out(i, j) = eucl_aux( m1(i, 0) - m2(j, 0),
+			    m1(i, 1) - m2(j, 1)  );
     }
   }
+  
   return out;
 }
 
@@ -40,6 +74,8 @@ arma::mat crossdist(const arma::mat& m1, const arma::mat& m2) {
 //' @param mat_list internal use
 //' @param mat_list1 internal use
 //' @param mat_list2 internal use
+//' @param x internal use
+//' @param y internal use
 //' @param return_single internal use
 //' @param pred_mat internal use
 //' @param x_to_list internal use
