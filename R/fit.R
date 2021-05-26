b5defe93d452656e57b1c64970f0e1fda44d6ae7
lcgodoy <lucasdac.godoy@gmail.com>
2021-05-26 13:39:28 -0400

    update delta method

--- R/fit.R
+++ R/fit.R
@@ -104,12 +104,13 @@ fit_spm.sspm <- function(x, model, theta_st,
     estimates <- op_val$par
     
     if(apply_exp) {
-        estimates[2:npar] <- c(expm1(estimates[2]), exp(estimates[3:npar]))
+        ## estimates[2:npar] <- c(expm1(estimates[2]), exp(estimates[3:npar]))
+        estimates[2:npar] <- exp(estimates[2:npar])
         ## Using Delta-Method
         ## https://stats.idre.ucla.edu/r/faq/how-can-i-estimate-the-standard-error-of-transformed-regression-parameters-in-r-using-the-delta-method/
         grad_mat <-
-            diag(c(1, exp(estimates[2]), estimates[3:npar]))
-        info_mat <- crossprod(grad_mat, info_mat) %*% info_mat
+            diag(c(1, exp(estimates[2:npar])))
+        info_mat <- crossprod(grad_mat, info_mat) %*% grad_mat
     } 
 
     output <- list(
